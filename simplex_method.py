65c0301d5d0109a6f4ade8a7c929c2c212891690
EHAT32 <enatik14@gmail.com>
2021-05-19 21:01:48 +0300

    Refactored variables

--- simplex_method.py
+++ simplex_method.py
@@ -1,10 +1,10 @@
-import re
 import numpy as np
 import sys
-import os
 import pandas as pd
 
 M = 1e5
+
+#Parsing from excel table, creating restrictions
 
 def parse_from_xlsx(file_path, variant):
     data_table_1 = pd.read_excel(file_path, header = 1, nrows = 19, usecols = range(1,7)).to_numpy()
@@ -24,6 +24,7 @@ def parse_from_xlsx(file_path, variant):
 
     return opt, restrictions
 
+
 def make_table(opt_func, restrictions):
     opt_func_variables, _ = opt_func
     variables_count = len(opt_func_variables) # count main variables
@@ -42,59 +43,59 @@ def make_table(opt_func, restrictions):
         table[-1, index - 1] = -multiplier
 
     extra_variables_offset = 0
-    for restrictionIdx in range(0, len(restrictions)):
-        variables, sign, value = restrictions[restrictionIdx]
+    for restriction_index in range(0, len(restrictions)):
+        variables, sign, value = restrictions[restriction_index]
         for var in variables:
             index, multiplier = var
-            table[restrictionIdx, index - 1] = multiplier
-        table[restrictionIdx, -1] = value
+            table[restriction_index, index - 1] = multiplier
+        table[restriction_index, -1] = value
         
         if sign == '<=':
-            table[restrictionIdx, variables_count + extra_variables_offset] = 1
+            table[restriction_index, variables_count + extra_variables_offset] = 1
             extra_variables_offset += 1
         elif sign == '>=':
-            table[restrictionIdx, variables_count + extra_variables_offset] = -1
+            table[restriction_index, variables_count + extra_variables_offset] = -1
             extra_variables_offset += 1
     return table
 
 def get_basis_vars_indices(table):
     basis_vars_indices = []
     basis_var_rows = []
-    for varIdx in range(np.size(table, 1) - 1):
-        col = table[:-1, varIdx]
+    for var_index in range(np.size(table, 1) - 1):
+        col = table[:-1, var_index]
         nnz = np.nonzero(col)[0]
         if len(nnz) == 1 and col[nnz[0]] == 1:
-            basis_vars_indices.append(varIdx)
+            basis_vars_indices.append(var_index)
             basis_var_rows.append(nnz[0])
     return basis_vars_indices, basis_var_rows
 
 def addMCoeffs(table):
     basis_vars_indices, _ = get_basis_vars_indices(table)
 
-    rowsIdsWithM = []
-    for rowIdx in range(np.size(table, 0) - 1):
-        need2AddMcoeff = True
-        for basisVarIdx in basis_vars_indices:
-            if table[rowIdx, basisVarIdx] != 0:
-                need2AddMcoeff = False
+    rows_indices_with_M = []
+    for row_index in range(np.size(table, 0) - 1):
+        need_2_add_M_coeff = True
+        for basis_var_index in basis_vars_indices:
+            if table[row_index, basis_var_index] != 0:
+                need_2_add_M_coeff = False
                 break
-        if need2AddMcoeff:
-            rowsIdsWithM.append(rowIdx)
+        if need_2_add_M_coeff:
+            rows_indices_with_M.append(row_index)
 
     rows, cols = table.shape
-    tableWithM = np.zeros((rows, cols + len(rowsIdsWithM)))
-    tableWithM[:, :cols - 1] = table[:, :cols - 1]
-    tableWithM[:rows - 1, -1] = table[:rows - 1, -1]
+    table_with_M = np.zeros((rows, cols + len(rows_indices_with_M)))
+    table_with_M[:, :cols - 1] = table[:, :cols - 1]
+    table_with_M[:rows - 1, -1] = table[:rows - 1, -1]
 
     offset = 0
-    for rowIdx in rowsIdsWithM:
-        for coefIdx in range(cols + offset):
-            tableWithM[-1, coefIdx] += tableWithM[rowIdx, coefIdx] * -M
-        tableWithM[-1, -1] += tableWithM[rowIdx, -1] * -M
-        tableWithM[rowIdx, cols - 1 + offset] = 1
+    for row_index in rows_indices_with_M:
+        for coef_index in range(cols + offset):
+            table_with_M[-1, coef_index] += table_with_M[row_index, coef_index] * -M
+        table_with_M[-1, -1] += table_with_M[row_index, -1] * -M
+        table_with_M[row_index, cols - 1 + offset] = 1
         offset += 1
 
-    return tableWithM
+    return table_with_M
 
 def check_optimal_solution(table):
     for var in table[-1, :-1]:
@@ -104,9 +105,9 @@ def check_optimal_solution(table):
 
 def check_solution_exist(table):
     rows, _ = table.shape
-    indexOfResolvingColumn = np.argmin(table[-1, :-1])
+    index_of_resolving_column = np.argmin(table[-1, :-1])
     for row in range(rows - 1):
-        if table[row, indexOfResolvingColumn] > 0:
+        if table[row, index_of_resolving_column] > 0:
             return True
     return False
 
@@ -121,57 +122,57 @@ def check_infinity_solutions(table):
     return False
 
 def simplex_iteration(table):
-    rows, cols = table.shape
-    minColumnIndex = np.argmin(table[-1, :-1])
+    rows, _ = table.shape
+    min_column_index = np.argmin(table[-1, :-1])
 
-    minRowIndex = -1
+    min_row_index = -1
     min = sys.float_info.max
 
     basis_vars_indices, _ = get_basis_vars_indices(table)
     basis_vars_indices = np.sort(basis_vars_indices)
     for row in range(rows - 1):
-        if (table[row, minColumnIndex] <= 0):
+        if (table[row, min_column_index] <= 0):
             continue
-        dividedValue = table[row, -1] / table[row, minColumnIndex]
+        divided_value = table[row, -1] / table[row, min_column_index]
 
-        if (abs(dividedValue - min) < sys.float_info.epsilon):
+        if (abs(divided_value - min) < sys.float_info.epsilon):
             for basis_var_index in basis_vars_indices:
                 if table[row, basis_var_index] == 1:
-                    minRowIndex = row
+                    min_row_index = row
                     break
 
-        if (dividedValue < min):
-            min = dividedValue
-            minRowIndex = row
+        if (divided_value < min):
+            min = divided_value
+            min_row_index = row
 
-    resolvingElement = table[minRowIndex, minColumnIndex]
-    newTable = np.zeros_like(table)
-    newTable[minRowIndex, :] = table[minRowIndex, :] / resolvingElement
+    resolving_element = table[min_row_index, min_column_index]
+    new_table = np.zeros_like(table)
+    new_table[min_row_index, :] = table[min_row_index, :] / resolving_element
 
     for row in range(rows):
-        if row == minRowIndex:
+        if row == min_row_index:
             continue
-        factor = table[row, minColumnIndex] / resolvingElement
-        newTable[row, :] = table[row, :] - table[minRowIndex, :] * factor
+        factor = table[row, min_column_index] / resolving_element
+        new_table[row, :] = table[row, :] - table[min_row_index, :] * factor
 
-    return newTable
+    return new_table
 
-def simplex_method(table, desiredIterations = 100, maximize = True):
+def simplex_method(table, desired_iterations = 100, maximize = True):
     result = np.copy(table)
     if not maximize:
         result[-1, :] *= -1
 
     iteration = 0
-    isSolutionExists = check_solution_exist(result)
-    isPlanOptimal = check_optimal_solution(result)
-    while (iteration < desiredIterations and not isPlanOptimal and isSolutionExists):
+    does_solution_exist = check_solution_exist(result)
+    is_plan_optimal = check_optimal_solution(result)
+    while (iteration < desired_iterations and not is_plan_optimal and does_solution_exist):
         result = simplex_iteration(result)
         iteration += 1
-        isPlanOptimal = check_optimal_solution(result)
-        if (not isPlanOptimal):
-            isSolutionExists = check_solution_exist(result)
+        is_plan_optimal = check_optimal_solution(result)
+        if (not is_plan_optimal):
+            does_solution_exist = check_solution_exist(result)
 
-    if not isSolutionExists:
+    if not does_solution_exist:
         print("Solution doesn't exist")
         return result
 
